243a7790440278a3162fc8455d55e85ece132d9b
Alexis-Crespo <crespo.a.alexis@gmail.com>
2024-10-23 12:26:00 -0300

    fix

--- src/components/ui/atoms/InputFile.tsx
+++ src/components/ui/atoms/InputFile.tsx
@@ -12,7 +12,7 @@ import {
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog";
-import Trash from "../icons/trash";
+import Trash from "../icons/Trash";
 import Upload from "../icons/Upload";
 import FileUpload from "../icons/FileUpload";
 import axios from "axios";
